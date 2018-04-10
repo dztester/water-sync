239694e4c1e67532bada7f9a16c550d66e9923a7
dzgrief <529265776@qq.com>
2018-04-10 08:38:14 +0000

    同步DMA分区和会员关系数据

---
+++ app/Console/Commands/SyncAreaMemberRelation.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Console\Commands;
+
+use GuzzleHttp\Client;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class SyncAreaMemberRelation extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'water-sync:sync-area-member-relation';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '同步DMA分区和会员关系数据';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $count = DB::table('hycsk.view_cust_meter')->count();
+        $limit = 100;
+        $times = ceil($count / $limit);
+
+        for ($i = 0; $i < $times; $i++) {
+
+            $offset = $i * $limit;
+
+            $records = DB::table('hycsk.view_cust_meter')->limit($limit)->offset($offset)->get();
+
+            $data_list = [];
+
+            foreach ($records as $record) {
+                $data_list[] = [
+                    'member_number' => $record->YHID ?? '',
+                    'area_name'     => $record->PQMC ?? '',
+                ];
+            }
+
+            $client = new Client(['base_uri' => 'http://182.61.56.51/']);
+            $response = $client->request('POST', 'area_member_relation', [
+                'json' => $data_list,
+            ]);
+        }
+    }
+}
